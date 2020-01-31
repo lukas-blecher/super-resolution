045c4e79fe36f10a3253cf0d3ac1c7c82789a4c7
Lukas <luk.blecher@gmx.de>
2020-01-31 15:35:35 +0100

    Update default.py

--- options/default.py
+++ options/default.py
@@ -2,7 +2,11 @@ import json
 from collections import namedtuple
 
 default_dict = {}
-with open('options/default.json') as f:
-    default_dict = json.load(f)
+try:
+    with open('options/default.json') as f:
+        default_dict = json.load(f)
+except FileNotFoundError:
+    with open('../options/default.json') as f:
+        default_dict = json.load(f)
 
 default = namedtuple("default", default_dict.keys())(*default_dict.values())
