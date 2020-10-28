04d8197ede4b6b9b27bf767b470a5cc6448fb702
unknown <f.keilbach@stud.uni-heidelberg.de>
2020-10-28 11:55:53 +0100

    0

--- utils.py
+++ utils.py
@@ -353,17 +353,17 @@ def plot_hist2d(sr, hr, cmap='jet',vmin=None,vmax=None):
     if vmax == None:
         vmax = max([hr.max().item(), sr.max().item()])
     f, ax = plt.subplots(1, 2)
-    plt.subplots_adjust(right=.75)
+    plt.subplots_adjust(right=.75, wspace=0.05)
     ax = ax.flatten()
     ax[0].imshow(sr, cmap=cmap, vmin=vmin, vmax=vmax)
-    ax[0].set_title('prediction')
+    ax[0].set_title('SR')
     ax[0].axis('off')
     gt = ax[1].imshow(hr, cmap=cmap, vmin=vmin, vmax=vmax)
-    ax[1].set_title('ground truth')
+    ax[1].set_title('HR')
     ax[1].axis('off')
     #f.subplots_adjust(right=0.8)
-    cbar_ax = f.add_axes([0.8, 0.25, 0.05, 0.5])
-    f.colorbar(gt, cax=cbar_ax, orientation="horizontal")
+    cbar_ax = f.add_axes([0.175, 0.25, 0.5, 0.025])
+    f.colorbar(gt, cax=cbar_ax, orientation="horizontal") 
     return f
 
 
