1e72c1f3ce6fd0285aa768597f6f013dd1edd119
FabianK27 <f.keilbach@stud.uni-heidelberg.de>
2020-10-28 19:33:44 +0100

    0

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -584,8 +584,8 @@ def distribution(dataset_path, dataset_type, generator, device, output_path=None
                         f.savefig((output_path+modes[m]).replace(".png", ""))
                     else:
                         if modes[m] == 'meanimg' and split_meanimg:
-                            for i in range(2):
-                                plt.savefig(output, format='pdf')
+                            #for i in range(2):
+                            plt.savefig(output, format='pdf')
                         else:
                             plt.savefig(output, format='pdf')
                         
